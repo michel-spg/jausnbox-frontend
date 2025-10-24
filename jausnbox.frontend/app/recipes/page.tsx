5c705620c4f14796c0e2cd37f25113d3493c098c
MIP <michel@spengergasse.at>
2025-10-24 11:31:17 +0200

    UPDATE content language

--- jausnbox.frontend/app/recipes/page.tsx
+++ jausnbox.frontend/app/recipes/page.tsx
@@ -46,7 +46,7 @@ export default function Recipes() {
             type="text"
             name="searchTerm"
             id="searchTerm"
-            placeholder="Search by name ..."
+            placeholder="Suche nach Titel ..."
             className="mb-2 w-full p-2 border border-gray-300 rounded-lg 
                      dark:bg-gray-800 dark:border-gray-700 dark:text-white
                      dark:placeholder-gray-400"
@@ -63,10 +63,10 @@ export default function Recipes() {
                 <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                 <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
               </svg>
-              <span>Loading recipes...</span>
+              <span>Lade Rezepte...</span>
             </div>
           ) : (
-            `Recipes: ${recipes ? recipes.length : 0}`
+            `Rezepte: ${recipes ? recipes.length : 0}`
           )}
         </div>
 
